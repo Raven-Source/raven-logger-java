cfa1ad7485e375a8ff3019d61cc1001167ea39af
yi.liang <indifer@126.com>
2021-09-08 17:13:08 +0800

    up version

--- raven-logger/src/main/java/org/raven/logger/JsonUtil.java
+++ raven-logger/src/main/java/org/raven/logger/JsonUtil.java
@@ -1,7 +1,5 @@
 package org.raven.logger;
 
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
-import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import net.logstash.logback.encoder.org.apache.commons.lang.StringUtils;
 import org.raven.serializer.withJackson.ObjectMapperConfig;
@@ -15,9 +13,10 @@ public final class JsonUtil {
 
     static {
         mapper = ObjectMapperConfig.getObjectMapper();
-        mapper.setVisibility(PropertyAccessor.SETTER, JsonAutoDetect.Visibility.NONE);
-        mapper.setVisibility(PropertyAccessor.GETTER, JsonAutoDetect.Visibility.NONE);
-        mapper.setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
+    }
+
+    public static void setMapper(ObjectMapper mapper) {
+        JsonUtil.mapper = mapper;
     }
 
     public static ObjectMapper getMapper() {
